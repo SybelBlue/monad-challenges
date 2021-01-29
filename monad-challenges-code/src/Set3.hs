819bc5ec4f991b7f8f5d6aab8a30f7321fcd3c76
L Caraco <lbc.dna@gmail.com>
2021-01-29 10:56:02 -0600

    allCards

--- monad-challenges-code/src/Set3.hs
+++ monad-challenges-code/src/Set3.hs
@@ -3,3 +3,14 @@ module Set3 where
 import MCPrelude
 
 import Prelude ()
+
+allPairs [] _ = []
+allPairs (x:xs) bs = zip (repeat  x) bs ++ allPairs xs bs
+
+data Card = Card Int String
+
+instance Show Card where
+    show (Card r s) = show r ++ s
+
+allCards :: [Int] -> [String] -> [Card]
+allCards rs ss = map (uncurry Card) $ allPairs rs ss
