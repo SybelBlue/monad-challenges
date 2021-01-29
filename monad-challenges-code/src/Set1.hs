73e2dbf88e2682aea0896d612140c89d4f8711a3
L Caraco <lbc.dna@gmail.com>
2021-01-28 18:05:02 -0600

    made generalA

--- monad-challenges-code/src/Set1.hs
+++ monad-challenges-code/src/Set1.hs
@@ -4,12 +4,14 @@ module Set1 where
 
 import MCPrelude
 import Data.Char (chr, ord)
+import Data.Tuple (fst)
 
 type Gen a = Seed -> (a, Seed)
 
 main = do
     putStrLn $ if fiveRandsCheck then "Ok!" else "Err " ++ show fiveRands
     putStrLn $ if randString3Check then "Ok!" else "Err " ++ show randString3
+    putStrLn $ if randEvenOddTenCheck then "Ok!" else "Err " ++ show randEvenOddTenCheck
 
 iterateRand1 = flip iterateRand (mkSeed 1)
 
@@ -26,15 +28,20 @@ randLetter = uncurry ((,) . chr . (+ ord 'a') . fromIntegral . (`mod` 26)) . ran
 randString3 = iterateNRand1 3 randLetter
 
 randEven :: Gen Integer 
-randEven s = let (v, n) = rand s in (2 * v, n)
+randEven = generalA rand (*2)
 
 randOdd :: Gen Integer 
-randOdd s = let (v, n) = randEven s in (v + 1, n)
+randOdd = generalA randEven succ
 
 randTen :: Gen Integer 
-randTen s = let (v, n) = rand s in (10 * v, n)
+randTen = generalA rand (*10)
+
+generalA f g s = let (v, n) = f s in (g v, n)
 
 fiveRandsCheck = checkProd == product fiveRands
     where checkProd = 8681089573064486461641871805074254223660
 
 randString3Check = randString3 == "lrf"
+
+randEvenOddTenCheck = checkProd == product (map (\f -> fst $ f $ mkSeed 1) [randEven, randOdd, randTen])
+    where checkProd = 189908109902700
